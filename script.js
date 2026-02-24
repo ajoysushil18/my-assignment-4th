615b734143a58ddcc65034a8064f8a79016ee27a
ajoysushil18 <ajoyshil412@gmail.com>
2026-02-24 20:44:20 +0600

    Add jobs data array and initial JavaScript setup

---
+++ script.js
@@ -0,0 +1,14 @@
+const jobs = [
+    {
+        id: 1,
+        companyName: "Google",
+        position: "Frontend Developer",
+        location: "California, USA",
+        type: "Full-time",
+        salary: "$120,000",
+        description: "Build scalable user interfaces.",
+        status: "all"
+    }
+];
+
+let currentTab = "all";
